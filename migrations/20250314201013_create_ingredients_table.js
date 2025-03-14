77b68ae965314167edf28d4c1187e3e5131ed351
Sydney <69093420+passportmidi@users.noreply.github.com>
2025-03-14 17:17:16 -0300

    add initial node project files and knex migration file

---
+++ migrations/20250314201013_create_ingredients_table.js
@@ -0,0 +1,24 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.createTable("ingredients", (table) => {
+    table.increments("id").primary();
+    table.string("name").notNullable();
+    table.string("volume").notNullable();
+    table.integer("grams").notNullable().defaultTo(0);
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table
+      .timestamp("updated_at")
+      .defaultTo(knex.raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"));
+  });
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.dropTable("ingredients");
+}
