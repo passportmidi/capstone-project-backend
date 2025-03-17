fd9667de8877e93353a273211925513c4f09aac7
Sydney <69093420+passportmidi@users.noreply.github.com>
2025-03-17 14:25:09 -0300

    add custom field to table

--- migrations/20250314201013_create_ingredients_table.js
+++ migrations/20250314201013_create_ingredients_table.js
@@ -8,6 +8,7 @@ export function up(knex) {
     table.string("name").notNullable();
     table.string("volume").notNullable();
     table.integer("grams").notNullable().defaultTo(0);
+    table.boolean("custom").notNullable().defaultTo(false);
     table.timestamp("created_at").defaultTo(knex.fn.now());
     table
       .timestamp("updated_at")
