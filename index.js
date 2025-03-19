8c09799c0ecc79ec8ea3c0f1a8e72cb2488f748b
Sydney <69093420+passportmidi@users.noreply.github.com>
2025-03-18 22:58:23 -0300

    minor edit: add CORS origin

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
+
 const app = express();
+
+const { CORS_ORIGIN } = process.env;
+
+app.use(cors({ origin: CORS_ORIGIN }));
 
 const PORT = process.env.PORT || 5050;
 
